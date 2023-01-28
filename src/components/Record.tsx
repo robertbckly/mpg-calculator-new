65e5bb3800be487dadba8e3abc14c21c3b213787
Robert Buckley <robqhd@gmail.com>
2023-01-28 14:24:29 +0000

    feat: improve record output formatting

--- src/components/Record.tsx
+++ src/components/Record.tsx
@@ -11,15 +11,21 @@ const UK_GALLON_LITRES = 4.5461;
 export default function Record({
   id, date, litres, miles, location, cost, onEdit, onDelete,
 }: RecordProps) {
-  const costPerLitre = (cost / litres).toFixed(3);
-  const costPerMile = (cost / miles).toFixed(2);
-  const milesPerGallon = (miles / (litres / UK_GALLON_LITRES)).toFixed(2);
+  const output = {
+    litres: Number(litres).toFixed(2),
+    miles: Number(miles).toFixed(2),
+    cost: Number(cost).toFixed(2),
+    costPerLitre: (cost / litres).toFixed(3),
+    costPerMile: (cost / miles).toFixed(2),
+    milesPerGallon: (miles / (litres / UK_GALLON_LITRES)).toFixed(2),
+  };
+
   return (
     <div className="record">
-      <p>{`[${date}] ${litres}L @ ${location} (£${cost}): ${miles}mi`}</p>
-      <p>{`= £${costPerLitre}/L`}</p>
-      <p>{`= £${costPerMile}/mi`}</p>
-      <p>{`= ${milesPerGallon}mpg`}</p>
+      <p>{`[${date}] ${output.litres}L @ ${location} (£${output.cost}): ${output.miles}mi`}</p>
+      <p>{`= £${output.costPerLitre}/L`}</p>
+      <p>{`= £${output.costPerMile}/mi`}</p>
+      <p>{`= ${output.milesPerGallon}mpg`}</p>
       <button type="button" onClick={() => onEdit(id)}>Edit</button>
       <button type="button" onClick={() => onDelete(id)}>Delete</button>
     </div>
