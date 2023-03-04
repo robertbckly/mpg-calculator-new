6a8421be7804e6d0fe2d98c77de66a578f8598e5
Robert Buckley <robertbckly@gmail.com>
2023-03-04 13:34:33 +0000

    refactor: fix broken `app` filename

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/app2';
+import App from './components/app';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
