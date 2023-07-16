2e3ac2bff4cac1b0af2c32d8b53f00ee914f4fcb
Robert Buckley <robertbckly@gmail.com>
2023-07-16 13:25:19 +0100

    refactor: StrictMode import

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/app/app';
 
 const root = createRoot(document.getElementById('root')!);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
