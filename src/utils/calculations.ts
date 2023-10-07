5a23b8dca385468decf1ccaf84a3827d690e0ce4
Rob Buckley <robertbckly@gmail.com>
2023-10-07 12:58:12 +0100

    chore: add note

--- src/utils/calculations.ts
+++ src/utils/calculations.ts
@@ -1,5 +1,11 @@
 import { LITRES_PER_GALLON } from '../constants/constants';
 
+/**
+ * Calculate MPG as fixed-point number with 2 fraction digits
+ * @param miles
+ * @param litres
+ * @returns mpg
+ */
 export function milesPerGallon(miles: number, litres: number) {
   return miles && litres
     ? (miles / (litres / LITRES_PER_GALLON)).toFixed(2)
